856b2a8929352cca7dc7bc40f4feb94fcc7941ce
Laura <lauracolof@gmail.com>
2023-01-23 17:56:56 -0300

    routes /pet done

--- src/routes/pets.router.ts
+++ src/routes/pets.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAllPets, getPetId, createPet } from "../controller/pet.controller";
+import { getAllPets, getPetId, createPet, updatePet, deletePet } from "../controller/pet.controller";
 
 const petsRouter = Router();
 
 petsRouter.get("/", getAllPets);
 petsRouter.get("/:id", getPetId);
 petsRouter.post("/", createPet);
+petsRouter.put('/:id', updatePet);
+petsRouter.delete('/:id', deletePet)
 
 export default petsRouter;
