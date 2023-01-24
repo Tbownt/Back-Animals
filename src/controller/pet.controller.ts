eaa2d75f4665099e4cb9bf549dc7f9db18b9163e
AleFalces <104443435+AleFalces@users.noreply.github.com>
2023-01-24 20:50:14 -0300

    relation USER/PET

--- src/controller/pet.controller.ts
+++ src/controller/pet.controller.ts
@@ -1,80 +1,84 @@
 import { Request, Response } from "express";
 import { Pet } from "../Model/Pet";
+import { User } from "../Model/User";
 
 export const getAllPets = async (req: Request, res: Response) => {
-  try {
-    const pets = await Pet.find();
-    res.status(200).send(pets);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+	try {
+		const pets = await Pet.find();
+		res.status(200).send(pets);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 };
 
 export const getPetId = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const pet = await Pet.findOneBy({ id });
-    if (!pet) res.status(400).send({ msg: "Pet Id not found" });
-    else res.status(200).send(pet);
-  } catch (error) {
-    res.status(404).send({ msg: "Error getting data" });
-  }
+	const { id } = req.params;
+	try {
+		const pet = await Pet.findOneBy({ id });
+		if (!pet) res.status(400).send({ msg: "Pet Id not found" });
+		else res.status(200).send(pet);
+	} catch (error) {
+		res.status(404).send({ msg: "Error getting data" });
+	}
 };
 
 export const createPet = async (req: Request, res: Response) => {
-  const { size, species, age, img, detail, area, sex, status, userId } =
-    req.body;
+	const { size, species, age, img, detail, area, sex, status, userId } =
+		req.body;
 
-  try {
-    const newPet = new Pet();
-    newPet.size = size;
-    newPet.species = species;
-    newPet.age = age;
-    newPet.img = img;
-    newPet.detail = detail;
-    newPet.area = area;
-    newPet.sex = sex;
-    newPet.status = status;
-    newPet.user = userId;
+	try {
+		const searchUser = await User.find({
+			where: [{ id: userId }],
+		});
 
-    await newPet.save();
-    // console.log(newPet)
-    return res.status(200).send(newPet);
-  } catch (error) {
-    if (error instanceof Error)
-      return res.status(400).json({ message: error.message });
-  }
+		const newPet = new Pet();
+		newPet.size = size;
+		newPet.species = species;
+		newPet.age = age;
+		newPet.img = img;
+		newPet.detail = detail;
+		newPet.area = area;
+		newPet.sex = sex;
+		newPet.status = status;
+		newPet.user = searchUser[0];
+
+		await newPet.save();
+		return res.status(200).send(newPet);
+	} catch (error) {
+		if (error instanceof Error)
+			return res.status(400).json({ message: error.message });
+	}
 };
 
 export const updatePet = async (req: Request, res: Response) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const pet = await Pet.findOneBy({ id: id });
-    if (!pet) return res.status(404).json({ message: "Pet not found" });
+	try {
+		const pet = await Pet.findOneBy({ id: id });
+		if (!pet) return res.status(404).json({ message: "Pet not found" });
 
-    await Pet.update({ id: id }, req.body);
-    return res.sendStatus(204);
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(500).json({ message: error.message });
-    }
-  }
+		await Pet.update({ id: id }, req.body);
+		return res.sendStatus(204);
+	} catch (error) {
+		if (error instanceof Error) {
+			return res.status(500).json({ message: error.message });
+		}
+	}
 };
 
 export const deletePet = async (req: Request, res: Response) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const result = await Pet.delete({ id: id });
+	try {
+		const result = await Pet.delete({ id: id });
 
-    if (result.affected === 0)
-      return res.status(404).json({ message: "Pet not found" });
+		if (result.affected === 0)
+			return res.status(404).json({ message: "Pet not found" });
 
-    return res.sendStatus(204).json({ message: "Pet deleted" });
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(500).json({ message: error.message });
-    }
-  }
+		return res.sendStatus(204).json({ message: "Pet deleted" });
+	} catch (error) {
+		if (error instanceof Error) {
+			return res.status(500).json({ message: error.message });
+		}
+	}
 };
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,56 +1,61 @@
 import { Request, Response, NextFunction } from "express";
+import { Pet } from "../Model/Pet";
 import { User } from "../Model/User";
 
 export const createUser = async (req: Request, res: Response) => {
-  const { name, surname, email, username, phone, role } = req.body;
-  try {
-    const newUser = new User();
-    newUser.name = name;
-    newUser.surname = surname;
-    newUser.email = email;
-    newUser.username = username;
-    newUser.phone = phone;
-    newUser.role = role;
+	const { name, surname, email, username, phone, role } = req.body;
+	try {
+		const newUser = new User();
+		newUser.name = name;
+		newUser.surname = surname;
+		newUser.email = email;
+		newUser.username = username;
+		newUser.phone = phone;
+		newUser.role = role;
 
-    await newUser.save();
-    // console.log(newUser);
+		await newUser.save();
+		// console.log(newUser);
 
-    res.status(200).send(newUser);
-  } catch (error) {
-    if (error instanceof Error)
-      return res.status(400).json({ message: error.message });
-  }
+		res.status(200).send(newUser);
+	} catch (error) {
+		if (error instanceof Error)
+			return res.status(400).json({ message: error.message });
+	}
 };
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await User.find();
-    res.status(200).send(users);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+	try {
+		const users = await User.find();
+		res.status(200).send(users);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 };
 
 export const getUserId = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const user = await User.findOneBy({ id });
-    if (!user) res.status(400).send({ msg: `User ${id} is not found` });
-    else res.status(200).send(user);
-  } catch (error) {
-    res.status(404).send({ msg: "Error getting data" });
-  }
+	const { id } = req.params;
+	try {
+		const user = await User.find({
+			where: [{ id: id }],
+			relations: ["pet"],
+		});
+
+		if (!user) res.status(400).send({ msg: `User ${id} is not found` });
+		else res.status(200).send(user);
+	} catch (error) {
+		res.status(404).send({ msg: "Error getting data" });
+	}
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { name, surname, email, username, phone } = req.body;
-  try {
-    const user = await User.findOneBy({ id: id });
-    if (!user) return res.status(404).json({ msg: `User ${id} is not found` });
-    await User.update({ id: id }, req.body);
-    res.status(200).send("User Updated");
-  } catch (error) {
-    res.status(404).send({ msg: "Error getting data" });
-  }
+	const { id } = req.params;
+	const { name, surname, email, username, phone } = req.body;
+	try {
+		const user = await User.findOneBy({ id: id });
+		if (!user) return res.status(404).json({ msg: `User ${id} is not found` });
+		await User.update({ id: id }, req.body);
+		res.status(200).send("User Updated");
+	} catch (error) {
+		res.status(404).send({ msg: "Error getting data" });
+	}
 };
