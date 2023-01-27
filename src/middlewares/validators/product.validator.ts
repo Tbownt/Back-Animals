623f770aa356d2bd1012dd8d4964565807756174
AleFalces <104443435+AleFalces@users.noreply.github.com>
2023-01-27 17:39:09 -0300

    product post validations

---
+++ src/middlewares/validators/product.validator.ts
@@ -0,0 +1,22 @@
+import { check, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+// checkear después imagen
+
+export const validatorPostProduct = [
+	check("name").exists().notEmpty().isLength({ min: 3, max: 20 }),
+
+	check("description").exists().notEmpty().isLength({ min: 8, max: 200 }),
+
+	check("price").exists().notEmpty().isNumeric(),
+
+	check("stock").exists().notEmpty().isNumeric(),
+
+	(req: Request, res: Response, next: NextFunction) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		return next();
+	},
+];
