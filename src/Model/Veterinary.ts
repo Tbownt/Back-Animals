3ae8d53b3b7968c1ef7ad637e1eb84c9d3d93072
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-02-02 14:37:19 -0400

    propiedad location agregada a model veterinary

--- src/Model/Veterinary.ts
+++ src/Model/Veterinary.ts
@@ -1,31 +1,33 @@
 import {
-	Entity,
-	Column,
-	PrimaryGeneratedColumn,
-	CreateDateColumn,
-	UpdateDateColumn,
-	BaseEntity,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
 } from "typeorm";
 
 @Entity()
 export class Veterinary extends BaseEntity {
-	@PrimaryGeneratedColumn("uuid")
-	id!: string;
-	@Column()
-	image!: string;
-	@Column()
-	name!: string;
-	@Column()
-	description?: string;
-	@Column()
-	phone!: number;
-	@Column()
-	address!: string;
-	@Column()
-	email!: string;
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+  @Column()
+  image!: string;
+  @Column()
+  name!: string;
+  @Column()
+  description?: string;
+  @Column()
+  phone!: number;
+  @Column("decimal", { array: true })
+  location?: string[];
+  @Column()
+  address!: string;
+  @Column()
+  email!: string;
 
-	@CreateDateColumn()
-	createdAt!: Date;
-	@UpdateDateColumn()
-	updatedAt!: Date;
+  @CreateDateColumn()
+  createdAt!: Date;
+  @UpdateDateColumn()
+  updatedAt!: Date;
 }
--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -2,20 +2,20 @@ import { Request, Response } from "express";
 import { User } from "../Model/User";
 
 export const formData = async (req: Request, res: Response) => {
-	const { email, username } = req.body;
-	try {
-		const user = await User.find({
-			where: [{ email: email }],
-			relations: ["pet"],
-		});
+  const { email, username } = req.body;
+  try {
+    const user = await User.find({
+      where: [{ email: email }],
+      relations: ["pet"],
+    });
 
-		if (user[0].username !== username)
-			return res.status(400).send("contraseña incorrecta");
-		else res.status(200).send(user);
-	} catch (error) {
-		if (error)
-			return res
-				.status(400)
-				.json("El Email ingresado no se encunetra registrado");
-	}
+    if (user[0].username !== username)
+      return res.status(400).send("contraseña incorrecta");
+    else res.status(200).send(user);
+  } catch (error) {
+    if (error)
+      return res
+        .status(400)
+        .json("El Email ingresado no se encuentra registrado");
+  }
 };
