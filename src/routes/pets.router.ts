af9205ccbf3e6ff68acce04804029055d6ff9c5d
AleFalces <104443435+AleFalces@users.noreply.github.com>
2023-02-03 15:41:02 -0300

    maldito auth0

--- src/routes/pets.router.ts
+++ src/routes/pets.router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import validatorPostPet from "../middlewares/validators/pet.validator";
 import {
-  getAllPets,
-  getPetId,
-  createPet,
-  updatePet,
-  deletePet,
+	getAllPets,
+	getPetId,
+	createPet,
+	updatePet,
+	deletePet,
 } from "../controller/pet.controller";
 // import checkJwt from "../utils/jwtAuth0";
 
@@ -13,7 +13,7 @@ const petsRouter = Router();
 
 petsRouter.get("/", getAllPets);
 petsRouter.get("/:id", getPetId);
-petsRouter.post("/", validatorPostPet, createPet);
+petsRouter.post("/", createPet);
 petsRouter.put("/:id", updatePet);
 petsRouter.delete("/:id", deletePet);
 
