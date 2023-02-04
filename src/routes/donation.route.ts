aff36ba4a3d36b5da5e90508191e03f112d1777e
Ignacio <ignacioCoriadb@gmail.com>
2023-02-04 20:39:54 -0300

    subscription mercadopago

--- src/routes/donation.route.ts
+++ src/routes/donation.route.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import {paymentMp} from "../controller/paymentMp";
+import { subscription } from "../controller/paymentMp";
 
 const donationRouter = Router();
 
 donationRouter.post('/', paymentMp );
-
-
-
-// donationRouter.get("/success", (req,res)=>{
-//     res.send("PAGO  EXITOSO")
-// })
+donationRouter.post("/subscription",subscription )
 
 
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,8 @@ import userRouter from "./user.router";
 import productRouter from "./product.router";
 import veterinaryRouter from "./veterinay.router";
 import donationRouter from "./donation.route";
+import subscription  from "./donation.route";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 //Usé el middleware Router() para crear manejadores de rutas montables y modularizados.
 const router = Router();
@@ -17,7 +19,7 @@ router.use("/veterinary", veterinaryRouter);
 
 
 router.use("/donation", donationRouter);
-
+router.use("/subscription", subscription);
 
 
 
